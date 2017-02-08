a26e0130ec9809ba3619c95d2bbee9f929589707
Pavel Rybin <drfisherozersk@gmail.com>
2017-02-08 17:24:02 +0300

    some changes

--- TvigleProxy.js
+++ TvigleProxy.js
@@ -5,24 +5,44 @@
 
 	var TvigleProxy = function () {
 		this.onLoaded = this.onLoaded.bind(this);
+		this.eventHandler = this.eventHandler.bind(this);
 	};
 
 	TvigleProxy.prototype.eventHandler = function (e) {
-		if (e.name == 'on_progress') {
-			// снижаем частоту до одного раза в 10 секунд
-			var progress = Math.floor(e.args[0] / 1000);
-			if (!(progress % 10) && this._prevProgress !== progress) {
-				this._prevProgress = progress;
-				console.log('on_progress:', progress);
-			}
-		} else {
-			console.log(e.name + ':', (e.args.length && e.args) || '');
+		switch (e.name) {
+
+			case 'on_progress': // на мобильной версии этого события нет
+				var progressInSeconds = Math.floor(e.args[0] / 1000);
+				// снижаем частоту до одного раза в 10 секунд
+				if (!(progressInSeconds % 10) && this._prevProgress !== progressInSeconds) {
+					this._prevProgress = progressInSeconds;
+					console.log('on_progress:', progressInSeconds);
+				}
+				break;
+
+			case 'start':
+			case 'playback_start': // на мобильной версии этого события нет
+				console.log('start');
+				setTimeout(getDuration.bind(null, this.id), 0);
+				break;
+
+			default:
+				console.log(e.name + ':', (e.args.length && e.args.join(', ')) || '');
 		}
 	};
 
 	TvigleProxy.prototype.onLoaded = function (id) {
 		console.log('onLoaded:', id);
+		this.id = id;
 	};
+
+	function getDuration (id) {
+		window.tvigle.api.getPlayer(id)
+			.getModule(window.tvigle.api.modules.APIModules.VIDEO_PLAYER)
+			.getDuration(function (duration) {
+				console.log('duration is', duration, 'sec');
+			});
+	}
 
 	window.TvigleProxy = new TvigleProxy();
 })();
